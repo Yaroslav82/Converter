2d3050ec65021fd75f60757160426619ddf28913
Yaroslav82 <yargay230607@gmail.com>
2023-06-09 22:16:05 +0300

    Add converting to kms

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
+        double miles = 15;
+        double kmsRes = convertMilesToKms(miles);
+        System.out.println(kmsRes + " kms in " + miles + " miles.");
+    }
+
+    private static double convertMilesToKms(double miles) {
+        return miles * 1.60934;
     }
 }
